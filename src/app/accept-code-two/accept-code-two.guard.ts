d53762d16f7ee91342a9db4f53a825e99b6da25b
schoes <sven.schoeni@gmail.com>
2022-11-28 20:55:37 +0100

    1 file modified

--- src/app/accept-code-two/accept-code-two.guard.ts
+++ src/app/accept-code-two/accept-code-two.guard.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class AcceptCodeTwoGuard implements CanActivate {
+  constructor(private readonly oidcService: OidcSecurityService) {}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.oidcService.checkAuth().pipe(
+      map((response) => {
+        console.log(response.idToken);
+        // here should the id token be adapted with the changes we made while we were at the IDP and it does not happen here
+        return response.isAuthenticated;
+      })
+    );
   }
 }
